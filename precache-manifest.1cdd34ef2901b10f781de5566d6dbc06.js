1ef79013066414fe697825b76cee076b0ed122b4
Michalik <jsmichal@bethel.jw.org>
2018-11-13 08:12:11 -0500

    Updates

---
+++ precache-manifest.1cdd34ef2901b10f781de5566d6dbc06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2a965a8e752f7cbc317",
+    "url": "/acts-of-apostles/static/js/runtime~main.d2a965a8.js"
+  },
+  {
+    "revision": "1e97946dfab2b8a4b75a",
+    "url": "/acts-of-apostles/static/js/main.1e97946d.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/acts-of-apostles/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "1e97946dfab2b8a4b75a",
+    "url": "/acts-of-apostles/static/css/main.ead2dcc2.chunk.css"
+  },
+  {
+    "revision": "0545464ab9fa39bfc91612878ab9e5f7",
+    "url": "/acts-of-apostles/index.html"
+  }
+];
